baf15c1ef6f272ee42fefcdfd5cc68144c4fa424
Josh Engle <josh@example.com>
2023-05-23 18:01:26 +0300

    Initial Commit

---
+++ mentorconnect/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Welcome to MentorConnect!")
+
+
+@csrf_exempt
+def bye(request):
+    return HttpResponse("Bye Bye from MentorConnect! Come back soon!")
