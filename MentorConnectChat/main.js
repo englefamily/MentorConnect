ef5244ce62ddb1efcbcdae64130ffa738c0a8963
Josh Engle <josh@example.com>
2023-05-25 16:53:33 +0300

    Fix the errors in the Chat app. It works now!!

--- MentorConnectChat/main.js
+++ MentorConnectChat/main.js
@@ -1,104 +1,109 @@
-const APP_ID = "de3b68dea39d4e2ea71981a4e5277def"
-const TOKEN = "007eJxTYNDgbvrGGpRckxd1cL81+7KNk5fzLM1s9/7xkMXho5Kg/zcFhpRU4yQzi5TURGPLFJNUo9REc0NLC8NEk1RTI3PzlNQ0lrq8lIZARoZ3he1MjAwQCOILMvim5hXllzjn5+WlJpc4ZySWMDAAALGxI8I="
-const CHANNEL = "MenrotConnectChat"
+const APP_ID = "de3b68dea39d4e2ea71981a4e5277def";
+const TOKEN =
+  "007eJxTYNDgbvrGGpRckxd1cL81+7KNk5fzLM1s9/7xkMXho5Kg/zcFhpRU4yQzi5TURGPLFJNUo9REc0NLC8NEk1RTI3PzlNQ0lrq8lIZARoZ3he1MjAwQCOILMvim5hXllzjn5+WlJpc4ZySWMDAAALGxI8I=";
+const CHANNEL = "MenrotConnectChat";
 
-const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
-let localTracks = []
-let remoteUsers = {}
+let localTracks = [];
+let remoteUsers = {};
 
 let joinAndDisplayLocalStream = async () => {
+  client.on("user-published", handleUserJoined);
 
-    client.on('user-published', handleUserJoined)
-    client.on('user-left', handleUserLeft)
+  client.on("user-left", handleUserLeft);
 
-    let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+  let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
 
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracls()
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
 
-    let player = `<div Class="video-continer" id="user-container-${UID}"> 
-                    <div class="video-player" id="user-${UID}"</div>
-                </div>`
-    document.getElementById('video-straems').insertAdjacentHTML('beforeend',  player)
+  let player = `<div class="video-container" id="user-container-${UID}">
+                        <div class="video-player" id="user-${UID}"></div>
+                  </div>`;
+  document
+    .getElementById("video-streams")
+    .insertAdjacentHTML("beforeend", player);
 
-    localTracks[1].play(`user-${UID}`)
+  localTracks[1].play(`user-${UID}`);
 
-    await client.publish([localTracks[0], localTracks[1]])
-}
+  await client.publish([localTracks[0], localTracks[1]]);
+};
 
 let joinStream = async () => {
-    await joinAndDisplayLocalStream()
-    document.getElementById('join-btn').style.display = 'none'
-    document.getElementById('stream-controls').style.display = 'flex'
-}
+  await joinAndDisplayLocalStream();
+  document.getElementById("join-btn").style.display = "none";
+  document.getElementById("stream-controls").style.display = "flex";
+};
 
 let handleUserJoined = async (user, mediaType) => {
-    remoteUsers[user.uid] = user
-    await client.subscribe(user, mediaType)
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
 
-    if (mediaType === 'video'){
-        let player = document.getElementById('user-container-${user.uid}')
-        if (player != null){
-            player.remove()
-        }
-
-        player = `<div class="video-container" id="user-container-${user.uid}">
-                    <div class="video-player" id="user-${user.uid}"></div>
-                 </div>`
-        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
-
-        user.videoTrack.play(`user-${user.uid}`)
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
     }
 
-    if (mediaType === 'audio'){
-        user.audioTrack.play()
-    }
-}
+    player = `<div class="video-container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                 </div>`;
+    document
+      .getElementById("video-streams")
+      .insertAdjacentHTML("beforeend", player);
+
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
 
 let handleUserLeft = async (user) => {
-    delete remoteUsers[user.uid]
-    document.getElementById(`user-container-${user.uid}`).remove()
-}
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+};
 
 let leaveAndRemoveLocalStream = async () => {
-    for(let i = 0; localTracks.length > i; i++){
-        localTracks[i].stop()
-        localTracks[i].close()
-    }
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
 
-    await client.leave()
-    document.getElementById('join-btn').style.display = 'block'
-    document.getElementById('stream-controls').style.display = 'none'
-    document.getElementById('video-streams').innerHTML = ''
-}
+  await client.leave();
+  document.getElementById("join-btn").style.display = "block";
+  document.getElementById("stream-controls").style.display = "none";
+  document.getElementById("video-streams").innerHTML = "";
+};
 
 let toggleMic = async (e) => {
-    if (localTracks[0].muted){
-        await localTracks[0].setMuted(false)
-        e.target.innerText = 'Mic on'
-        e.target.style.backgroundColor = 'cadetblue'
-    }else{
-        await localTracks[0].setMuted(true)
-        e.target.innerText = 'Mic off'
-        e.target.style.backgroundColor = '#EE4B2B'
-    }
-}
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    e.target.innerText = "Mic on";
+    e.target.style.backgroundColor = "cadetblue";
+  } else {
+    await localTracks[0].setMuted(true);
+    e.target.innerText = "Mic off";
+    e.target.style.backgroundColor = "#EE4B2B";
+  }
+};
 
 let toggleCamera = async (e) => {
-    if (localTracks[1].muted){
-        await localTracks[1].setMuted(false)
-        e.target.innerText = 'Camera on'
-        e.target.style.backgroundColor = 'cadetblue'
-    }else{
-        await localTracks[1].setMuted(true)
-        e.target.innerText = 'Camera off'
-        e.target.style.backgroundColor = '#EE4B2B'
-    }
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+    e.target.innerText = "Camera on";
+    e.target.style.backgroundColor = "cadetblue";
+  } else {
+    await localTracks[1].setMuted(true);
+    e.target.innerText = "Camera off";
+    e.target.style.backgroundColor = "#EE4B2B";
+  }
+};
 
-}
-
-
-document.getElementById('join-btn').addEventListener('click', joinStream)
-document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
-document.getElementById('mic-btn').addEventListener('click', toggleMic)
-document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById("join-btn").addEventListener("click", joinStream);
+document
+  .getElementById("leave-btn")
+  .addEventListener("click", leaveAndRemoveLocalStream);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
