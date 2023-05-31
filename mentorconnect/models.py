7efdc00c672d8af9e4897fd2fc296cc6439adaec
hananel <g.hananel20@gmail.com>
2023-05-31 13:31:03 +0300

    Rebasing agreed upon code to `master`

--- mentorconnect/models.py
+++ mentorconnect/models.py
@@ -1,180 +1,130 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.password_validation import validate_password
 from django.core.validators import MaxValueValidator, MinValueValidator
+from .helphers import AGE_CHOICES, CITIES_CHOICES
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
-class UserModel(models.Model):
-    email = models.EmailField(max_length=50, null=False, blank=True, db_index=True)
-    password = models.CharField(max_length=30, validators=[validate_password], null=False)
-    #  TODO: Password validation to ensure complexity level for security. Possibly also validate emails
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
 
-    def __str__(self):
-        return self.email
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Mentor(models.Model):
-    # Really would only relevant if used here
-    # identity_number = models.CharField(null=False, max_length=9, db_index=True)
+    gender = models.CharField(choices=[('male', 'male'), ('female', 'female')], max_length=128)
     first_name = models.CharField(null=False, max_length=50)
     last_name = models.CharField(null=False, max_length=50)
-    #  TODO: Change to DOB with dropdown list:
-    age = models.IntegerField(null=False)
-    #  TODO: Change to city with dropdown list CITY_CHOICES:
-    address = models.CharField(null=False, max_length=128)
-    about_me = models.CharField(null=False, max_length=256) # Space to write about experience, education etc...
-    #  TODO: profile_pic = .... upload picture routine ....
-    user = models.OneToOneField(UserModel, on_delete=models.RESTRICT, related_name='mentor', null=False)
-    students = models.ManyToManyField('Student', related_name='mentors')
-    courses = models.ManyToManyField('Course', related_name='mentors')
-    mentor_hourly_rate = models.DecimalField(max_digits=3, decimal_places=2, null=False)
-    #  TODO: link to `Payment` & `Report` classes?
+    year_of_birth = models.CharField(choices=AGE_CHOICES, null=False, max_length=4)
+    address_city = models.CharField(choices=CITIES_CHOICES, max_length=128)
+    short_description = models.CharField(null=False, max_length=256)
+    long_description = models.CharField(null=False, max_length=700)
+    teach_online = models.BooleanField(null=False, default=False)
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, related_name='mentor', null=False)
+    students = models.ManyToManyField('Student', related_name='mentors', null=True, blank=True)
+    sub_topics = models.ManyToManyField('LearningSubTopic', related_name='mentors')
 
     class Meta:
         db_table = 'mentor'
 
     def __str__(self):
-        # If we use TZ number
-        # return f"ID: {self.pk} person: {self.first_name} {self.last_name} TZ: {self.identity_number}"
-
-        # Otherwise:
         return f"ID: {self.pk} Mentor: {self.first_name} {self.last_name}"
 
 
+
 class Student(models.Model):
-    # Really would only relevant if used here
-    # identity_number = models.CharField(null=False, max_length=9, db_index=True)
     first_name = models.CharField(null=False, max_length=50)
     last_name = models.CharField(null=False, max_length=50)
-    #  TODO: Change to DOB with dropdown list:
-    age = models.IntegerField(null=False)
-    #  TODO: Change to city with dropdown list CITY_CHOICES:
-    address = models.CharField(null=False, max_length=128)
-    about_me = models.CharField(null=False, max_length=64) # brief description, ex. `12th Grade @ x High School`
-    #  TODO: profile_pic = .... upload picture routine ....
-    user = models.OneToOneField(UserModel, on_delete=models.RESTRICT, related_name='student', null=False)
-    mentors = models.ManyToManyField('Mentor', related_name='students')
-    courses = models.ManyToManyField('Course', related_name='students')
+    year_of_birth = models.CharField(choices=AGE_CHOICES, null=False, max_length=4)
+    address_city = models.CharField(choices=CITIES_CHOICES, max_length=128)
+    short_description = models.CharField(null=False, max_length=256) # brief description, ex. `12th Grade @ x High School`
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, related_name='student', null=False)
+    sub_topics = models.ManyToManyField('LearningSubTopic', related_name='students',  blank=True)
+    # mentors = models.ManyToManyField('Mentor', related_name='students')
 
     class Meta:
         db_table = 'student'
 
     def __str__(self):
-        # If we use TZ number
-        # return f"ID: {self.pk} person: {self.first_name} {self.last_name} TZ: {self.identity_number}"
-
-        # Otherwise:
         return f"ID: {self.pk} Student: {self.first_name} {self.last_name}"
 
 
-class CourseCategory(models.Model):
-    course_category = models.CharField(null=False, max_length=50)
-
-
-class Course(models.Model):
-    course_title = models.CharField(null=False, max_length=50)
-    course_desc = models.CharField(null=False, max_length=128)
-    category = models.ForeignKey(CourseCategory, on_delete=models.RESTRICT, related_name='courses', null=False)
+class LearningTopic(models.Model):
+    topic_name = models.CharField(null=False, max_length=50)
 
     class Meta:
-        db_table = 'course'
+        db_table = 'topic'
 
     def __str__(self):
-        return f"ID: {self.pk} Course: {self.course_title} Description: {self.course_desc}"
+        return f"ID: {self.pk} Sub topic name: {self.topic_name}"
+
+
+class LearningSubTopic(models.Model):
+    sub_topic_name = models.CharField(null=False, max_length=50)
+    topic = models.ForeignKey(LearningTopic, on_delete=models.RESTRICT, related_name='courses', null=False)
+
+    class Meta:
+        db_table = 'sub_topic'
+
+    def __str__(self):
+        return f"ID: {self.pk} Sub topic name: {self.sub_topic_name}"
 
 
 class FeedBack(models.Model):
-    fb_title = models.CharField(null=False, max_length=50)
-    fb_content = models.CharField(null=False, max_length=128)
+    fb_content = models.CharField(null=False, max_length=228)
     fb_stars = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    student = models.OneToOneField(Student, on_delete=models.CASCADE, related_name='feedback')
-    mentor = models.OneToOneField(Mentor, on_delete=models.CASCADE, related_name='feedback')
-    course = models.OneToOneField(Course, on_delete=models.CASCADE, related_name='feedback')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='feedbacks')
+    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name='feedbacks')
+    sub_topic = models.CharField(max_length=100, choices=[], null=True, blank=True)
 
     class Meta:
         db_table = 'feedback'
 
     def __str__(self):
-        return f"ID: {self.pk} Feedback for {self.mentor}, {self.course}: {self.fb_title} - {self.fb_content}. " \
-               f"From: {self.student}"
+        return f"ID: {self.pk} Feedback: {self.fb_content}"
 
+    # def save(self, *args, **kwargs):
+    #     mentor_sub_topics = self.mentor.sub_topics.all()
+    #     self._meta.get_field('sub_topic').choices = [(topic.sub_topic_name, topic.sub_topic_name) for topic in mentor_sub_topics]
+    #     super(FeedBack, self).save(*args, **kwargs)
 
-class Chat(models.Model):
-    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name='chats')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='chats')
-    message = models.CharField(max_length=256, null=False)
-    #  TODO: course_work = .... logic for send/ receive files ....
-    timestamp = models.DateTimeField(auto_now_add=True)
+    def __init__(self, *args, **kwargs):
+        mentor_sub_topics = self.mentor.sub_topics.all()
+        self._meta.get_field('sub_topic').choices = [('a', 'a'), ('b', 'b')]
+        super().__init__(*args, **kwargs)
 
-    class Meta:
-        db = 'chat'
-
-
-class TimeTable(models.Model):
-    MENTOR_AVAILABILITY = [
-        ('Available', 'Available'),
-        ('Not Available', 'Not Available'),
-    ]
-    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name='timetables')
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='timetables')
-    # Make a `booking` `intermediate table` to track which student booked the mentor's available block? See below...
-    start_time = models.DateTimeField(null=False)
-    end_time = models.DateTimeField(null=False)
-    availability = models.CharField(choices=MENTOR_AVAILABILITY, max_length=13, null=False)
-
-    class Meta:
-        db_table = 'timetable'
-
-# Todo: decide if needed.
-# `intermediate table` continued... Looks like good idea to me - but is it needed?
-# class Booking(models.Model):
-#     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-#     timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
-#     booked_at = models.DateTimeField(auto_now_add=True)
-#
-# class Meta:
-#     db_table = 'booking'
-
-
-# This is just temporary. Just realising how difficult this will be to consider different banks worldwide.
-# Todo: Check with `Stripe` API to see what info is needed and if they provide the `model`s
-class Accounting(models.Model):
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='accounting')
-    card_holder_name = models.CharField(max_length=50, null=False)
-    card_number = models.IntegerField(max_length=16, null=False)
-    cvc_number = models.IntegerField(max_length=3, null=False)
-
-    class Meta:
-        db_table = 'accounting'
-
-
-# This too is temporary.
-# Todo: Check with `Stripe` API...
-class Payment(models.Model):
-    PAYMENT_CHOICES = [
-        ('Pending', 'Pending'),
-        ('Paid', 'Paid'),
-        ('Failed', 'Failed'),
-    ]
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='payments')
-    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name='payments')
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='payments')
-    amount = models.DecimalField(max_digits=4, decimal_places=2, null=False)
-    status = models.CharField(choices=PAYMENT_CHOICES, max_length=20, null=False)
-
-    class Meta:
-        db_table = 'payment'
-
-
-# Todo: payment_due/ received should be linked to `Payment` class?
-class Report(models.Model):
-    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE, related_name='reports')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='reports')
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='reports')
-    duration = models.DurationField(null=False)
-    payment_due = models.DecimalField(max_digits=4, decimal_places=2, null=False)
-    payment_received = models.DecimalField(max_digits=4, decimal_places=2, null=False)
-
-    class Meta:
-        db_table = 'report'
