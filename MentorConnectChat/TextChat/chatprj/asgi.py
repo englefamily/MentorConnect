8eaab445803884d56c8418c88dde259b5678f143
Josh Engle <josh@example.com>
2023-06-28 00:51:50 +0300

    Finally found my problem - Basic Chat now works!! More advancements to come...

--- MentorConnectChat/TextChat/chatprj/asgi.py
+++ MentorConnectChat/TextChat/chatprj/asgi.py
@@ -17,8 +17,8 @@ import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatprj.settings')
 
 application = ProtocolTypeRouter({
-    'http':get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
