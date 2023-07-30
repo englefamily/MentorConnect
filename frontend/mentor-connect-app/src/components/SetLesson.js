98ae85071d80c9b1b6bd9d1cbab6b526d358def1
hananel <g.hananel20@gmail.com>
2023-07-30 18:38:11 +0300

    add scroll and pagination

--- frontend/mentor-connect-app/src/components/SetLesson.js
+++ frontend/mentor-connect-app/src/components/SetLesson.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import context from "../Context";
 
 function SetLesson() {
+  const { userData } = useContext(context);
+
   return (
     <div>
       set lesson
+      {userData.user_id}
+      {/* <DropDown
+        subjects={true}
+        placeholder="עיר\אזור"
+        objects={CITIES_CHOICES}
+        name="city_residence"
+        value={mentorData.city_residence}
+        onChange={handleChange}
+      /> */}
     </div>
-  )
+  );
 }
 
-export default SetLesson
+export default SetLesson;
